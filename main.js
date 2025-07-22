a1dd5537f915704c0285c6d2105f6bc836b6849f
vlad <vladislove0509@gmail.com>
2025-07-22 13:15:57 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+// var frist_name = prompt('Введите своё имя');
+// var age = prompt('Введите сколько вам лет');
+// var primer1 = prompt('сколько будет 2 + 1 = ');
+// var primer2 = prompt('сколько будет 9 - 2 = ');
+// var primer3 = prompt('сколько будет 4 * 2 = ');
+// var primer4 = prompt('сколько будет 6 / 3 = ');
+// var primer5 = prompt('сколько будет 10 % 3 = ');
+// var res = alert('зайдите в консоль, чтобы увидеть результаты');
+// console.log('Вас зовут' + frist_name + ', вам ' + age + ' лет');
+// console.log('Пример 1: 2 + 1 = 3, ваш ответ: ' + primer1);
+// console.log('Пример 2: 9 - 2 = 7, ваш ответ: ' + primer2);
+// console.log('Пример 3: 4 * 2 = 8, ваш ответ: ' + primer3);
+// console.log('Пример 4: 6 / 3 = 2, ваш ответ: ' + primer4);
+// console.log('Пример 5: 10 % 3 = 1, ваш ответ: ' + primer5);
+
+
+
+
+
+
+
+
+let x = Number(prompt("Введите число X:"));
+let y = Number(prompt("Введите число Y:"));
+let z = Number(prompt("Введите число Z:"));
+let res = (x + y + z) / 3;
+
+
+alert("Среднее арифметическое: " + res);
